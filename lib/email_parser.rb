3bf242558bc29c62201970b9a169fa3a7bc02fd6
gyeon <gyeon19@gmail.com>
2016-06-14 16:29:56 -0400

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,3 +1,23 @@
+require 'pry'
+class EmailParser
+  attr_accessor :email
+
+  def initialize(email_new)
+    @email = email_new
+  end
+ 
+  def parse
+    @email.delete(",").split.uniq
+  end
+  # first error - need to delete the comma
+  # got: ["avi@test.com,", "arel@test.com"]
+
+  #second error - need to remove duplicate emails
+  # got: ["avi@test.com", "avi@test.com"]
+  #USE UNIQ!
+
+end
+
 # Build a class EmailParser that accepts a string of unformatted 
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
